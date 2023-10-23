6f8176d9caee07f7f789f056f1e6619af40cd81b
SUBASHINI N <142592205+suba18rajan@users.noreply.github.com>
2023-10-23 20:31:10 +0530

    Main File

---
+++ main.py
@@ -0,0 +1,10 @@
+from login import Login_System
+from dashboard import IMS
+from tkinter import *
+
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = Login_System(root)
+    root.mainloop()
+
